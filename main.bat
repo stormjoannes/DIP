b939bab161aa246c8db3da34d730f23847254a22
stormjoannes <stormjoannes10@hotmail.com>
2021-05-13 15:28:33 +0200

    files in folders

--- main.bat
+++ main.bat
@@ -1,7 +1,7 @@
-type data\Dutch_text.txt | python mapper.py | python reducer.py
-echo Dutch | python right_filename.py
+type data\Dutch_text.txt | python algorithm\mapper.py | python algorithm\reducer.py
+echo Dutch | python algorithm\right_filename.py
 
-type data\English_text.txt | python mapper.py | python reducer.py
-echo English | python right_filename.py
+type data\English_text.txt | python algorithm\mapper.py | python algorithm\reducer.py
+echo English | python algorithm\right_filename.py
 
-python Language_guesser.py
+python algorithm\Language_guesser.py
